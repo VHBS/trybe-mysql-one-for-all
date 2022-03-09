f415432fc0c2c3a543db6e130cf82dbd3bb9058e
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-03-09 11:58:55 -0300

    feat: Requisito 4 - Query para descobrir se o usuario esteve ativo no ano de 2021 baseado na data mais recente da coluna "historicos_de_reproducoes"

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+	u.usuario AS usuario,
+    IF(MAX(h.data_de_reproducao) LIKE '2021%', 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+		FROM SpotifyClone.usuarios AS u
+		JOIN SpotifyClone.historicos_de_reproducoes AS h
+        ON u.usuario_id = h.usuario_id
+GROUP BY usuario
+ORDER BY usuario
