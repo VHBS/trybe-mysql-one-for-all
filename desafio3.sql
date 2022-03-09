2b37da2976c2ba595ab5ef29c2260b4202125da9
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-03-09 11:39:44 -0300

    feat: Requisito 3 - Query para exibir as colunas "usuario", "qtde_musicas_ouvidas" e "total_minutos" e seus valores

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT 
+	u.usuario AS usuario, 
+	COUNT(c.cancao) AS qtde_musicas_ouvidas,
+    ROUND(SUM(duracao_segundos/ 60), 2) AS total_minutos
+		FROM SpotifyClone.usuarios AS u 
+		JOIN SpotifyClone.historicos_de_reproducoes AS h
+		ON u.usuario_id = h.usuario_id
+		JOIN SpotifyClone.cancoes AS c
+		ON h.cancao_id = c.cancao_id
+GROUP BY u.usuario
+ORDER BY u.usuario;
